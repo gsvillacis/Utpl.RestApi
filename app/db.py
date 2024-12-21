fd22d958e32321b64b963ffabf41bcf6593b9b98
gsvillacis <gsvillacis@utpl.edu.ec>
2024-12-21 05:49:37 +0000

    bdd6

--- app/db.py
+++ app/db.py
@@ -1,7 +1,5 @@
 import os
-
 from sqlmodel import create_engine, SQLModel, Session
-
 
 database_url = "postgresql://userincidentes:CW5LAfeBwyzq97xlH9F8ueaKaIsR5jg0@dpg-ctj1d5t2ng1s73bg1in0-a.oregon-postgres.render.com/dbincidentes_gcfp"
 
@@ -14,7 +12,9 @@ engine = create_engine(database_url, echo=True)
 def init_db():
     SQLModel.metadata.create_all(engine)
 
-# Función para obtener la sesión de la base de datos
-    def get_session():
-        with Session(engine) as session:
-            yield session
+# Función para obtener una sesión de la base de datos
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
