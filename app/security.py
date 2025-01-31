3accc42766c955fa8f4d68ba38c05f0676ff10a2
gsvillacis <gsvillacis@utpl.edu.ec>
2025-01-31 23:21:38 +0000

    security

---
+++ app/security.py
@@ -0,0 +1,36 @@
+
+# importación de librerías
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+# creación objeto security tipo HTTPBasic
+
+security = HTTPBasic()
+
+# Lista de usuarios con sus credenciales y token de autenticación
+
+users = {
+    "admin": {
+        "password": "PassGVDB",
+        "token": "",
+        "priviliged": True
+    }
+}
+
+# Función para validar las credenciales de los usuarios
+
+
+def verification(creds: HTTPBasicCredentials = Depends(security)):
+    username = creds.username
+    password = creds.password
+
+       if username in users and password == users[username]["password"]:
+            print('usuario autenticado')
+            return True
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Credenciales inválidas",
+                headers={"WWW-Authenticate": "Basic"},
+            )
