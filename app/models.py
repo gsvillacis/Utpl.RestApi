6b1de5d68dc2c522643fc2825f374398b5109c32
gsvillacis <gsvillacis@utpl.edu.ec>
2025-02-01 02:22:42 +0000

    utils

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, Field
-from typing import Optional
-from pydantic import BaseModel
-from datetime import date, time
+from typing import Optional, List, Dict
+from pydantic import BaseModel, EmailStr
+from datetime import datetime, date
 
 
 class Incidente(SQLModel, table=True):
@@ -15,3 +15,49 @@ class Incidente(SQLModel, table=True):
     Estado: str = "Nuevo"
     Prioridad: str = "P4"
     Resolución: Optional[str] = None
+
+
+# Clases para el manejo de usuarios
+
+
+class GetUser(BaseModel):
+    email: EmailStr
+    username: Optional[str]
+    role: str
+
+    class Config:
+        orm_mode = True
+        use_enum_values = True
+
+
+class LoginUser(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        orm_mode = True
+        use_enum_values = True
+
+
+class PostUser(BaseModel):
+    email: EmailStr
+    username: Optional[str]
+    password: str
+
+    class Config:
+        orm_mode = True
+        use_enum_values = True
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(unique=True, index=True)
+    email: str = Field(unique=True, index=True)
+    hashed_password: str
+    role: str = Field(default="user")
+
+
+class Token(SQLModel, table=True):
+    id: int = Field(primary_key=True, index=True)
+    token: str = Field(index=True)
+    user_id: int
